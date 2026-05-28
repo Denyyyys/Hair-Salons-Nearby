fe96fb6e42b0a70e96406bec3b959b29465e5963
Denyyyys <denys.fokashchuk@gmail.com>
2026-05-28 14:48:29 +0200

    backend: Setup backend project

---
+++ backend/src/main/kotlin/denyyyys/salons_nearby/SalonsNearbyApplication.kt
@@ -0,0 +1,11 @@
+package denyyyys.salons_nearby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalonsNearbyApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalonsNearbyApplication>(*args)
+}
---
+++ backend/src/test/kotlin/denyyyys/salons_nearby/SalonsNearbyApplicationTests.kt
@@ -0,0 +1,13 @@
+package denyyyys.salons_nearby
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalonsNearbyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
