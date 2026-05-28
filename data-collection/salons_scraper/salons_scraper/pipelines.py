55121e14d48f0937ae1fca757486ba2a57b3e7f0
Denyyyys <denys.fokashchuk@gmail.com>
2026-05-28 14:31:24 +0200

    data-collection: Create pipeline for storing scraped data in the MongoDB

--- data-collection/salons_scraper/salons_scraper/pipelines.py
+++ data-collection/salons_scraper/salons_scraper/pipelines.py
@@ -1,13 +1,51 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from pathlib import Path
+from pymongo.errors import ServerSelectionTimeoutError
+
+load_dotenv(
+    Path(__file__).resolve().parents[3] / ".env"
+)
+
+class MongoPipeline:
+    def __init__(self):
+        mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+        mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        mongo_port = os.getenv("MONGO_PORT", "27017")
+        mongo_uri = (f"mongodb://{mongo_user}:{mongo_password}@localhost:{mongo_port}")
+
+        try:
+            self.client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
+            self.client.admin.command("ping")
+        except ServerSelectionTimeoutError as e:
+            raise RuntimeError(f"Could not connect to MongoDB: {e}")
+
+        self.db = self.client["salons"]
+        self.collection = self.db["salons"]
+        self.collection.create_index("booksy_business_id", unique=True)
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
-class SalonsScraperPipeline:
     def process_item(self, item, spider):
+        print("ITEM IS BEING PROCESSED")
+        item_dict = dict(item)
+
+        self.collection.update_one(
+            {
+                "booksy_business_id":
+                    item_dict[
+                        "booksy_business_id"
+                    ]
+            },
+            {
+                "$set": item_dict
+            },
+            upsert=True
+        )
+
         return item
+
+    def close_spider(self, spider):
+        self.client.close()
+
