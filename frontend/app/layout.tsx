cefcbf441c6654d3b1a7b61e7e49e8a8b648c11d
Denyyyys <denys.fokashchuk@gmail.com>
2026-05-30 21:14:45 +0200

    frontend: Add toastify package and init setup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import ToastProvider from "@/components/ToastProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         <div className="flex-1">{children}</div>
+        <ToastProvider />
       </body>
     </html>
   );
