1ee8ada46183061d927d1a5fca4b30ad4c161442
Denyyyys <denys.fokashchuk@gmail.com>
2026-05-30 21:50:56 +0200

    frontend: Setup docker compose for frontend

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,2 +1,8 @@
+const CLIENT_API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8085";
+
+const SERVER_API_BASE_URL =
+  process.env.INTERNAL_API_BASE_URL ?? "http://backend:8085";
+
 export const API_BASE_URL =
-    process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8085";
+  typeof window === "undefined" ? SERVER_API_BASE_URL : CLIENT_API_BASE_URL;
